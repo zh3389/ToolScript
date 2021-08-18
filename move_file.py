12415668ca1f0cd3d7d09a1d8453e6eb5b7fbdb5
zh <zh@nosugartech.com>
2021-08-18 15:54:00 +0800

    feat: 添加文件移动脚本

---
+++ move_file.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+from tqdm import tqdm
+
+"""
+移动指定文件夹下的所有下一级下下级下下下...级文件 到指定指定的路径下.
+"""
+
+move_dir = "data"
+g = os.walk(move_dir)
+for path, dir_list, file_list in g:
+    for file_name in tqdm(file_list):
+        if str(file_name)[:2] == "._":
+            continue
+        if str(file_name)[0] == ".":
+            os.remove(os.path.join(path, file_name))
+            continue
+        if path != move_dir:
+            shutil.move(os.path.join(path, file_name), move_dir)
