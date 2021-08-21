cb9b45b9eec3fc1d0382de2b916ef8bac2b63464
zh <zh3389@gmail.com>
2021-08-22 00:11:01 +0800

    feat: 增加环境搭建的说明.

--- encryption/pyarmor_license.sh
+++ encryption/pyarmor_license.sh
@@ -1,3 +1,5 @@
+# 环境搭建
+# pip install pyarmor
 # 加时间许可加密代码
 pyarmor licenses --expired 2021-08-21 --bind-mac f0:18:98:e8:bc:e7 --bind-dis C07844202QWKXPGA6 --fixed 1 ./
 pyarmor obfuscate --with-license licenses/license.lic main.py -r
